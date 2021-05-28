8ec3eebd04eeea0331caad03086bdd263d0778e6
Benito Städel <benito.staedel@pm.me>
2021-05-28 12:48:13 +0200

    Refactor;  dark-mode support

--- ChatBubbles.user.js
+++ ChatBubbles.user.js
@@ -1,277 +1,248 @@
 // ==UserScript==
-// @name         Chat Bubbles for Torn
-// @namespace    https://github.com/Pi77Bull
-// @version      1.0
-// @description  Makes the chat look like an ordinary messenger app.
-// @author       Pi77Bull [2082618]
-// @match        https://www.torn.com/*
-// @grant        GM_addStyle
-// @run-at       document-start
+// @name          Chat Bubbles for Torn
+// @namespace     https://github.com/Pi77Bull
+// @match         https://www.torn.com/*
+// @grant         GM_addStyle
+// @grant         GM_getValue
+// @grant         GM_setValue
+// @version       2.0.0
+// @author        Pi77Bull [2082618]
+// @run-at        document-start
+// @inject-into   content
 // ==/UserScript==
 
-let targetNode = document.querySelector("html");
+/* Change false to true in the line below to edit the settings: */
+const edit = false;
+/* Don't forget to reset it to false again after you're finished. */
 
-let config = {
-    childList: true,
-    subtree: true
+/* Change the values below to your preferences. */
+if (edit) {
+	GM_setValue('config', {
+		bubbleColor: '#dddddd',
+		bubbleColorDarkMode: '#454545',
+
+		meBubbleColor: '#2162a7',
+		meBubbleColorDarkMode: '#37464f',
+
+		fontSize: '1.3em',
+		fontFamily: 'Ubuntu, Segoe UI',
+	});
+}
+
+const WAIT_FOR = (selector, config = { attributes: true, childList: true, subtree: true }, parent = document) => {
+	return new Promise((resolve) =>
+		new MutationObserver((mutations, observer) => {
+			let selectedNode = parent.querySelector(selector);
+			if (selectedNode) {
+				resolve(selectedNode);
+				observer.disconnect;
+			}
+		}).observe(document.documentElement, config)
+	);
 };
 
-let msgObserver = new MutationObserver((mutationsList, observer) => {
-    for (let mutation of mutationsList) {
-        if (mutation.addedNodes.length && mutation.addedNodes[0].classList) {
-            if (mutation.target.classList.contains("chat-box_Wjbn9")) { // chat maximized
-                styleMessages(mutation.addedNodes[0].querySelectorAll(".message_oP8oM"));
-                let viewport = mutation.addedNodes[0].querySelector(".viewport_1F0WI");
-                viewport.scrollTo(0, viewport.scrollHeight);
-                break;
-            } else if (mutation.addedNodes[0].classList.contains("message_oP8oM")) { // new message
-                styleMessages(mutation.addedNodes);
-                break;
-            } else if (mutation.target.classList.contains("chat-settings-opts_1p3VN")) { // settings maximized first time
-                let div = document.createElement("div");
-                div.innerHTML = `<div class="setting-header">Chat Bubbles Settings</div><div class="setting-item"><div class="chat-opt-label_1t2AS t-gray-9 bold">My messages BG color</div><div class="setting-value"><input type="text" id="meColorText" class="text-input" maxlength="7" value="${settings.meColor}"><input type="color" id="meColor" class="color-input" value="${settings.meColor}"></div></div><div class="setting-item"><div class="chat-opt-label_1t2AS t-gray-9 bold">Font-Size</div><div class="setting-value"><input type="range" id="fontSize" min="12" max="20" value="${settings.fontSize.slice(0, -2)}"></div></div><div class="setting-item"><div class="chat-opt-label_1t2AS t-gray-9 bold">Font-Family</div><div class="setting-value"><input type="text" class="text-input" value="${settings.fontFamily}" id="fontFamily"></div></div>`;
-                div.id = "chatbubbles-settings";
-                document.querySelector(".chat-box-settings_Ogzjk .overview_1MoPG").appendChild(div);
-                let meColor = document.querySelector("#meColor");
-                let meColorText = document.querySelector("#meColorText");
-                meColor.addEventListener("input", () => {
-                    settings.meColor = meColor.value;
-                    meColorText.value = meColor.value;
-                    localStorage.setItem("chatbubbles", JSON.stringify(settings));
-                    setCSSVARS({
-                        "meColor": settings.meColor
-                    });
-                });
-                meColorText.addEventListener("input", () => {
-                    settings.meColor = meColorText.value;
-                    meColor.value = meColorText.value;
-                    localStorage.setItem("chatbubbles", JSON.stringify(settings));
-                    setCSSVARS({
-                        "meColor": settings.meColor
-                    });
-                });
+const config = { childList: true, subtree: true };
 
-                let fontSize = document.querySelector("#fontSize");
-                fontSize.addEventListener("input", () => {
-                    settings.fontSize = fontSize.value + "px";
-                    localStorage.setItem("chatbubbles", JSON.stringify(settings));
-                    setCSSVARS({
-                        "fontSize": settings.fontSize
-                    });
-                });
-
-                let fontFamily = document.querySelector("#fontFamily");
-                fontFamily.addEventListener("input", () => {
-                    settings.fontFamily = fontFamily.value;
-                    localStorage.setItem("chatbubbles", JSON.stringify(settings));
-                    setCSSVARS({
-                        "fontFamily": settings.fontFamily
-                    });
-                });
-                break;
-            }
-        }
-    }
+const observer = new MutationObserver((mutationsList) => {
+	mutationsList.forEach((mutation) => {
+		for (let node of mutation.addedNodes) {
+			const classList = [...node.classList];
+			if (classList.some((className) => className.startsWith('message_'))) {
+				// new message
+				classifyMessages([node]);
+			} else if (
+				classList.some((className) => className.startsWith('chat-box-content_')) &&
+				![...mutation.target.classList].some((className) => className.startsWith('chat-box-settings_'))
+			) {
+				// chat maximized
+				classifyMessages(node.querySelectorAll('[class^="message_"], [class*=" message_"]'));
+				break;
+			}
+		}
+	});
 });
 
+let userid;
 let username;
-
-let observer = new MutationObserver(() => {
-    if (!username && document.querySelector("script[name]")) {
-        username = document.querySelector("script[name]").getAttribute("name");
-    }
-
-    if (document.querySelector(".chat-box-wrap_20_R_")) {
-
-
-        msgObserver.observe(document.querySelector(".chat-box-wrap_20_R_"), config);
-
-        styleMessages(document.querySelectorAll(".message_oP8oM")); // chats already maximized
-        document.querySelectorAll(".viewport_1F0WI").forEach((e, i) => { // scroll all maximized chats to bottom
-            e.scrollTo(0, e.scrollHeight);
-        });
-
-        observer.disconnect();
-    }
+WAIT_FOR('[class^="chat-box-wrap_"], [class*=" chat-box-wrap_"]', config).then((element) => {
+	const userElement = document.body.querySelector('script[uid]');
+	userid = userElement.getAttribute('uid');
+	username = userElement.getAttribute('name');
+	classifyMessages(document.querySelectorAll('[class^="message_"], [class*=" message_"]'));
+	observer.observe(element, config);
 });
-observer.observe(targetNode, config);
 
-function styleMessages(messages) {
-    for (let m of messages) {
-        let nodes = m.parentNode.childNodes;
-        let i = Array.prototype.indexOf.call(nodes, m);
-        let sender = nodes[i].querySelector("a").innerText.trim().replace(":", "");
-        /*
-                if (messages.length == 1) {
-                    nodes[i].classList.add("animate");
-                }
-        */
-        if (i > 0 && nodes[i - 1].querySelector("a").innerText.trim().replace(":", "") == sender) {
-            nodes[i - 1].classList.remove("bubbleend");
-            nodes[i].classList.add("bubbleend", "samesender");
-        } else {
-            nodes[i].classList.add("bubbleend", "bubblestart");
-        }
+function classifyMessages(messageElements) {
+	messageElements.forEach((messageElement) => {
+		messageElement.classList.add('bubbleend');
+		messageElement.querySelector('a').textContent = messageElement.querySelector('a').textContent.replace(':', '');
 
-        if (nodes[i].querySelector("a").innerText.trim().replace(":", "") == username) {
-            nodes[i].classList.add("me");
-        }
+		console.log(username);
+		if (messageElement.querySelector('a').href.endsWith(userid)) {
+			messageElement.classList.add('me');
+		} else if (messageElement.querySelector('span').innerText.includes(username)) {
+			messageElement.classList.add('mention');
+		}
 
-        if (nodes[i].querySelector("span").innerText.includes(username)) {
-            nodes[i].classList.add("mention");
-        }
-
-        nodes[i].querySelector("a").innerText = nodes[i].querySelector("a").innerText.trim().replace(":", "");
-    }
+		if (messageElement.previousElementSibling?.querySelector('a').href === messageElement.querySelector('a').href) {
+			messageElement.previousElementSibling.classList.remove('bubbleend');
+			messageElement.classList.add('samesender');
+		}
+	});
 }
 
-function setCSSVARS(obj) {
-    for (let cssVar in obj) {
-        document.documentElement.style.setProperty(`--${cssVar}`, obj[cssVar]);
-    }
+const settings = GM_getValue('config', {
+	bubbleColor: '#dddddd',
+	bubbleColorDarkMode: '#454545',
+
+	meBubbleColor: '#2162a7',
+	meBubbleColorDarkMode: '#37464f',
+
+	fontSize: '1.3em',
+	fontFamily: 'Ubuntu, Segoe UI',
+});
+
+const lightColor = '#ffffff';
+const darkColor = '#333333';
+
+const fontColor = getBrightness(settings.bubbleColor) > 127.5 ? darkColor : lightColor;
+const fontColorDarkMode = getBrightness(settings.bubbleColorDarkMode) > 127.5 ? darkColor : lightColor;
+const meFontColor = getBrightness(settings.meBubbleColor) > 127.5 ? darkColor : lightColor;
+const meFontColorDarkMode = getBrightness(settings.meBubbleColorDarkMode) > 127.5 ? darkColor : lightColor;
+
+function getBrightness(colorString) {
+	// https://awik.io/determine-color-bright-dark-using-javascript/
+	// https://stackoverflow.com/questions/596216/formula-to-determine-perceived-brightness-of-rgb-color
+	const el = document.createElement('xyz');
+	el.style.backgroundColor = colorString;
+	const [, r, g, b, a] = el.style.backgroundColor.match(/^rgba?\((\d+), (\d+), (\d+)(?:, (\d+(?:\.\d+)))?\)$/);
+	const brightness = Math.sqrt(0.299 * r ** 2 + 0.587 * g ** 2 + 0.114 * b ** 2);
+	el.remove();
+	return brightness;
 }
-
-
-let defaults = {
-    meColor: "#9fddf9",
-    fontSize: "16px",
-    fontFamily: "Arial"
-};
-let settings = JSON.parse(localStorage.getItem("chatbubbles"));
-
-if (!settings) { // settings don't exist
-    settings = defaults;
-} else { // settings exist
-    for (let e in defaults) {
-        if (!settings.hasOwnProperty(e)) { // fill settings with missing defaults
-            settings[e] = defaults[e];
-        }
-    }
-}
-localStorage.setItem("chatbubbles", JSON.stringify(settings));
-
-setCSSVARS(settings);
-
-
-
-
 
 GM_addStyle(`
-.viewport_1F0WI {
-    overflow-x: hidden;
-    position: relative;
-}
+	#chatRoot {
+		--bubbleColor: ${settings.bubbleColor};
+		--bubbleColorDarkMode: ${settings.bubbleColorDarkMode};
 
-.chat-box_Wjbn9 .error_22pJp,
-.chat-box_Wjbn9 .overview_1MoPG>div:last-child {
-    clear: both;
-    text-align: center;
-}
+		--meBubbleColor: ${settings.meBubbleColor};
+		--meBubbleColorDarkMode: ${settings.meBubbleColorDarkMode};
 
-.message_oP8oM {
-    float: left;
-    clear: both;
-    border-top-left-radius: 4px;
-    border-top-right-radius: 16px;
-    border-bottom-left-radius: 4px;
-    border-bottom-right-radius: 16px;
-    border: 1px solid transparent;
-    width: auto;
-    min-width: 1.2em;
-    max-width: 95%;
-    padding: 0 !important;
-    margin: 2px 2px;
-    background: white;
-    box-shadow: 0px 2px 2px 0px rgba(50, 50, 50, 0.3);
-    font-size: var(--fontSize);
-    font-family: var(--fontFamily);
-}
+		--fontSize: ${settings.fontSize};
+		--fontFamily: ${settings.fontFamily};
+		
+		--fontColor: ${fontColor};
+		--fontColorDarkMode: ${fontColorDarkMode};
+		--meFontColor: ${meFontColor};
+		--meFontColorDarkMode: ${meFontColorDarkMode};
+	}
+`);
 
-.message_oP8oM>a {
-    display: block;
-    margin: 0.2em 0.4em -0.2em 0.4em;
-}
+GM_addStyle(`
+	[class^='overview_'],
+	[class*=' overview_'] {
+		display: flex !important;
+		flex-direction: column !important;
+		height: 100%;
+		box-sizing: border-box;
+	}
 
-.message_oP8oM>span {
-    margin: 0.2em 0.4em 0.2em 0.4em;
-    display: block;
-}
+	[class^='message_'],
+	[class*=' message_'] {
+		display: flex;
+		flex-direction: column;
+		border-top-left-radius: 0.25em;
+		border-bottom-left-radius: 0.25em;
+		border-top-right-radius: 1em;
+		border-bottom-right-radius: 1em;
+		background-color: var(--bubbleColor);
+		border: 1px solid transparent;
+		padding: 4px 6px 6px 6px !important;
+		margin: 4px;
+		width: max-content;
+		max-width: calc(100% - 22px);
+		box-shadow: 0px 2px 2px 0px rgba(50, 50, 50, 0.3);
+		line-height: 1em !important;
+		font-size: var(--fontSize);
+		font-family: var(--fontFamily);
+	}
 
-.message_oP8oM.me {
-    float: right;
-    border-top-left-radius: 16px;
-    border-top-right-radius: 4px;
-    border-bottom-left-radius: 16px;
-    border-bottom-right-radius: 4px;
-    background: var(--meColor);
-}
+	.dark-mode [class^='message_'],
+	.dark-mode [class*=' message_'] {
+		background-color: var(--bubbleColorDarkMode);
+	}
 
-.message_oP8oM.bubbleend,
-.message_oP8oM.bubbleend.me {
-    border-bottom-left-radius: 16px;
-    border-bottom-right-radius: 16px;
-}
+	[class^='message_']:first-of-type,
+	[class*=' message_']:first-of-type {
+		margin-top: 0;
+	}
 
-.message_oP8oM:not(.bubblestart) {
-    margin-top: 0;
-}
+	[class^='message_'] > a,
+	[class*=' message_'] > a {
+		width: max-content;
+	}
 
-.message_oP8oM.me>a,
-.message_oP8oM.samesender>a {
-    display: none;
-}
+	[class^='message_'] a,
+	[class*=' message_'] a {
+		color: inherit !important;
+	}
 
-.mention {
-    background: lightgreen;
-}
+	[class^='message_'].me,
+	[class*=' message_'].me {
+		align-self: flex-end;
+		border-top-left-radius: 1em;
+		border-bottom-left-radius: 1em;
+		border-top-right-radius: 0.25em;
+		border-bottom-right-radius: 0.25em;
+		background-color: var(--meBubbleColor);
+		color: var(--meFontColor);
+	}
 
-.staff_1NUHz>a::after {
-    content: "Staff";
-    float: right;
-    font-size: 0.6em;
-    font-family: monospace;
-    color: #d83500;
-    text-transform: uppercase;
-}
+	[class^='message_'].me > a,
+	[class*=' message_'].me > a {
+		display: none;
+	}
 
-.chat-box-content_2C5UJ .chat-opt-label_1t2AS {
-    width: auto !important;
-}
+	.dark-mode [class^='message_'].me,
+	.dark-mode [class*=' message_'].me {
+		background-color: var(--meBubbleColorDarkMode);
+		color: var(--meFontColorDarkMode);
+	}
 
-.setting-header {
-    margin: 5px;
-    text-decoration: underline;
-    font-size: 14px;
-    font-weight: bold;
-    color: #999999;
-}
+	[class^='message_'].bubbleend,
+	[class*=' message_'].bubbleend {
+		border-bottom-left-radius: 1em;
+		border-bottom-right-radius: 1em;
+	}
 
-.setting-item {
-    display: flex;
-    align-items: center;
-    width: 100%;
-    margin-bottom: 5px;
-}
+	[class^='message_'].samesender,
+	[class*=' message_'].samesender {
+		margin-top: 0;
+	}
 
-.setting-value {
-    margin-left: auto;
-    min-width: 150px;
-    display: flex;
-    align-items: center;
-}
+	[class^='message_'].samesender > a,
+	[class*=' message_'].samesender > a {
+		display: none;
+	}
 
-.text-input {
-    height: 2em;
-    width: 60px;
-    text-align: center;
-    font-family: monospace !important;
-    border: 1px solid black;
-    flex-grow: 1;
-}
+	[class^='message_'].mention,
+	[class*=' message_'].mention {
+		border: 1px solid;
+	}
 
-.color-input {
-    padding: 0;
-    width: 32px;
-    height: 32px;
-}
-`)
+	[class^='chat-box-content_'],
+	[class*=' chat-box-content_'] {
+		color: var(--fontColor) !important;
+	}
+
+	.dark-mode [class^='chat-box-content_'],
+	.dark-mode [class*=' chat-box-content_'] {
+		color: var(--fontColorDarkMode) !important;
+	}
+
+	[class^='chat-last-message-label_'],
+	[class*=' chat-last-message-label_'] {
+		margin-top: auto !important;
+	}
+`);
--- README.md
+++ README.md
@@ -1,21 +1,22 @@
 # Chat-Bubbles-for-Torn
 
 ## Requirements
-You will need a userscript manager to install this script. I recommend using Tampermonkey.
 
-[Tampermonkey for Firefox](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey)
-
-[Tampermonkey for Chrome](https://chrome.google.com/webstore/detail/dhdgffkkebhmkfjojejmpbldmpobfkfo)
+You will need a userscript manager to install this script. I recommend using [Violentmonkey](https://violentmonkey.github.io/get-it/).
 
 ## Download
 
-[Install Chat Bubbles for Torn](https://github.com/Pi77Bull/Chat-Bubbles-for-Torn/raw/master/ChatBubbles.user.js)
+[Install Chat Bubbles for Torn](https://github.com/Pi77Bull/Chat-Bubbles-for-Torn/blob/master/ChatBubbles.user.js?raw=true)
 
-## Screenshots
+## Screenshot
 
-![Torn Chat & Settings](https://i.imgur.com/Yj3e7rY.png)
+![Preview](https://github.com/Pi77Bull/Chat-Bubbles-for-Torn/blob/master/preview.png?raw=true)
+
+## User preferences
+
+Setting are now available inside the script. After the installation, use your userscript manager to edit the script, then follow the instructions written on the first couple lines.
 
 ## Bugs, issues and ideas
 
-If you find any issues, have questions or ideas send me a message on Torn: 
+If you find any issues, have questions or ideas send me a message on Torn:
 **[Pi77Bull [2082618]](https://www.torn.com/profiles.php?XID=2082618)**
